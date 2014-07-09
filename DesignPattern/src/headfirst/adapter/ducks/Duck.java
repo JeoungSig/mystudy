74702305e52d4b586d7695430143f152d6c51da3
Steven Min <steven.jsmin@gmail.com>
2014-07-09 19:29:50 +1000

    Test 001

--- DesignPattern/src/headfirst/adapter/ducks/Duck.java
+++ DesignPattern/src/headfirst/adapter/ducks/Duck.java
@@ -1,6 +1,13 @@
 package headfirst.adapter.ducks;
 
+/**
+ * This is Duck interface class
+ * 
+ * @author Steven J.S Min
+ * 
+ */
 public interface Duck {
 	public void quack();
+
 	public void fly();
 }
